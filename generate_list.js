d1f7da0ec6ac31080546cc163baa206a5145fe75
Thomas Daede <daede003@umn.edu>
2014-08-27 14:50:31 -0700

    Read info.json files into master run list

--- generate_list.js
+++ generate_list.js
@@ -7,7 +7,13 @@ fs.readdirSync('runs').forEach(function(run_id) {
   job.run_id = run_id;
   job.tasks = fs.readdirSync('runs/'+run_id);
   var stat = fs.statSync('runs/'+run_id);
+  var info = {};
+  try {
+    var infoFile = fs.readFileSync('runs/'+run_id+'/info.json');
+    info = JSON.parse(infoFile);
+  } catch (e) {};
   job.date = stat.mtime;
+  job.info = info;
   jobs.push(job);
 });
 
