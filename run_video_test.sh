1bae434336ead9ea8346064acf664af235f4c2be
Thomas Daede <daede003@umn.edu>
2015-04-29 14:04:58 -0700

    Let EXTRA_OPTIONS pass through as an environment variable.

--- run_video_test.sh
+++ run_video_test.sh
@@ -7,4 +7,4 @@ set -e
 mkdir -p "runs/$2/$3"
 
 cd rd_tool
-DAALA_ROOT=../daala python -u rd_tool.py -codec $CODEC -extra-options "$EXTRA_OPTIONS" -prefix "../runs/$2/$3" "$3"
+DAALA_ROOT=../daala python -u rd_tool.py -codec $CODEC -prefix "../runs/$2/$3" "$3"
