9adaaa9e6a445a1452dd7462153e129b6e6b2ccf
Thomas Daede <daede003@umn.edu>
2015-05-21 15:00:18 -0700

    Output watermark info in graph_over_time.py

--- graph_over_time.py
+++ graph_over_time.py
@@ -9,6 +9,9 @@ import datetime
 
 runs = json.load(open("list.json","r"))
 
+watermark_runs = ['x265_1.6_ntt-short-1','x264_ntt']
+watermark_data = {}
+
 psnr = []
 psnrhvs = []
 ssim = []
@@ -20,19 +23,30 @@ def unix_time(dt):
     return delta.total_seconds()
 
 for run in runs:
-  if (u'master' in run['info']) and (u'ntt-short-1' in run['info']['task']):
+  is_watermark_run = run['run_id'] in watermark_runs
+  is_master_run = (u'master' in run['info']) and (u'ntt-short-1' in run['info']['task'])
+  if is_watermark_run or is_master_run:
     filename = 'runs/'+run['run_id']+'/'+run['info']['task']+'/total.out'
     try:
       distortion = subprocess.check_output(['./distortion.m',filename,'0.1'])
-      date_str = subprocess.check_output(['git','--git-dir=daala/.git','--work-tree=daala/','show','-s','--format=%ci',run['info']['commit']])
-      date_obj = dateutil.parser.parse(date_str).replace(tzinfo=None)
-      date_js = unix_time(date_obj) * 1000
-      psnr.append([date_js,distortion.split('\n')[0]])
-      psnrhvs.append([date_js,distortion.split('\n')[1]])
-      ssim.append([date_js,distortion.split('\n')[2]])
-      fastssim.append([date_js,distortion.split('\n')[3]])
+      if is_master_run:
+        date_str = subprocess.check_output(['git','--git-dir=daala/.git','--work-tree=daala/','show','-s','--format=%ci',run['info']['commit']])
+        date_obj = dateutil.parser.parse(date_str).replace(tzinfo=None)
+        date_js = unix_time(date_obj) * 1000
+        psnr.append([date_js,distortion.split('\n')[0]])
+        psnrhvs.append([date_js,distortion.split('\n')[1]])
+        ssim.append([date_js,distortion.split('\n')[2]])
+        fastssim.append([date_js,distortion.split('\n')[3]])
+      if is_watermark_run:
+        watermark_run = run['info']['task']
+        watermark_data[watermark_run] = {}
+        watermark_data[watermark_run]['psnr'] = distortion.split('\n')[0]
+        watermark_data[watermark_run]['psnrhvs'] = distortion.split('\n')[1]
+        watermark_data[watermark_run]['ssim'] = distortion.split('\n')[2]
+        watermark_data[watermark_run]['fastssim'] = distortion.split('\n')[3]
     except subprocess.CalledProcessError:
       continue
 
 output = [psnr, psnrhvs, ssim, fastssim]
 json.dump(output,open('time_series.json','w'))
+json.dump(watermark_data,open('watermark.json','w'))
