966abc9fedaf8b35083719a463d27f96ef9a3e23
Thomas Daede <daede003@umn.edu>
2016-05-22 20:40:12 -0700

    Support running bd_rate on bare directories

--- bd_rate_report.py
+++ bd_rate_report.py
@@ -5,11 +5,13 @@ from scipy import *
 from scipy.interpolate import interp1d
 from scipy.interpolate import pchip
 import sys
+import os
 import argparse
 import json
 
 parser = argparse.ArgumentParser(description='Produce bd-rate report')
 parser.add_argument('run',nargs=2,help='Run folders to compare')
+parser.add_argument('--anchor',help='Explicit anchor to use')
 parser.add_argument('--anchordir',nargs=1,help='Folder to find anchor runs')
 parser.add_argument('--suffix',help='Metric data suffix (default is .out)',default='.out')
 args = parser.parse_args()
@@ -47,38 +49,48 @@ def bdrate(file1, file2, anchorfile):
         ret[m] = bdr
     return ret
 
-
-info_data = {}
-info_data[0] = json.load(open(args.run[0]+'/info.json'))
-info_data[1] = json.load(open(args.run[1]+'/info.json'))
-
-if info_data[0]['task'] != info_data[1]['task']:
-    print("Runs do not match.")
-    sys.exit(1)
-
-task = info_data[0]['task']
-
-sets = json.load(open("rd_tool/sets.json"))
-videos = sets[task]["sources"]
-
-info_data[2] = json.load(open(args.anchordir[0]+'/'+sets[task]['anchor']+'/info.json'))
-
-if info_data[2]['task'] != info_data[0]['task']:
-    print("Mismatched anchor data!")
-    sys.exit(1)
-
 metric_data = {}
-for video in videos:
-    metric_data[video] = bdrate(args.run[0]+'/'+task+'/'+video+args.suffix,args.run[1]+'/'+task+'/'+video+args.suffix,args.anchordir[0]+'/'+sets[task]['anchor']+'/'+task+'/'+video+args.suffix)
+
+try:
+    info_data = {}
+    info_data[0] = json.load(open(args.run[0]+'/info.json'))
+    info_data[1] = json.load(open(args.run[1]+'/info.json'))
+
+    if info_data[0]['task'] != info_data[1]['task']:
+        print("Runs do not match.")
+        sys.exit(1)
+
+    task = info_data[0]['task']
+
+    sets = json.load(open("rd_tool/sets.json"))
+    videos = sets[task]["sources"]
+
+    info_data[2] = json.load(open(args.anchordir[0]+'/'+sets[task]['anchor']+'/info.json'))
+
+    if info_data[2]['task'] != info_data[0]['task']:
+        print("Mismatched anchor data!")
+        sys.exit(1)
+
+    for video in videos:
+        metric_data[video] = bdrate(args.run[0]+'/'+task+'/'+video+args.suffix,args.run[1]+'/'+task+'/'+video+args.suffix,args.anchordir[0]+'/'+sets[task]['anchor']+'/'+task+'/'+video+args.suffix)
+except FileNotFoundError:
+    # no info.json, using bare directories
+    if not args.anchor:
+        print("You must specify an anchor to use if comparing bare result directories.")
+        exit(1)
+    videos= os.listdir(args.anchor)
+    for video in videos:
+        metric_data[video] = bdrate(args.run[0]+'/'+video,args.run[1]+'/'+video,args.anchor+'/'+video)
 
 filename_len = 40
 for video in videos:
     if filename_len < len(video):
         filename_len = len(video)
 print("AWCY Report v0.4")
-print('Reference: ' + info_data[0]['run_id'])
-print('Test Run: ' + info_data[1]['run_id'])
-print('Range: Anchor ' + info_data[2]['run_id'] + ' q range 20-50')
+if info_data:
+    print('Reference: ' + info_data[0]['run_id'])
+    print('Test Run: ' + info_data[1]['run_id'])
+    print('Range: Anchor ' + info_data[2]['run_id'] + ' q range 20-50')
 avg = {}
 for m in range(0,len(met_name)):
     avg[m] = mean([metric_data[x][m] for x in metric_data])
