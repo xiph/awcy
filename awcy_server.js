4ea994ef410fd8fcf20543267a910812c83d8681
Thomas Daede <daede003@umn.edu>
2016-06-27 13:53:26 -0700

    Change cooldown time to 10 minutes

--- awcy_server.js
+++ awcy_server.js
@@ -158,7 +158,7 @@ function pollAmazon() {
     autoScalingGroups = data;
   });
   if ((!job_in_progress) && (job_queue.length == 0)) {
-    var shutdown_threshold = 1000*60*5; // 5 minutes
+    var shutdown_threshold = 1000*60*10; // 10 minutes
     if ((Date.now() - last_job_completed_time) > shutdown_threshold) {
       console.log("Shutting down all Amazon instances because idle.");
       shutdownAmazon();
