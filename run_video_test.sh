37820db84b1e4b8b73c68b9a5bbe7d0dbea72d0d
Thomas Daede <daede003@umn.edu>
2016-06-29 15:56:55 -0700

    Split out codec building into its own script.

--- run_video_test.sh
+++ run_video_test.sh
@@ -6,56 +6,9 @@ set -e
 
 mkdir -p "runs/$2/$3"
 
-#export PATH=/sbin:/bin:/usr/sbin:/usr/bin
-echo $PATH
-echo $HOME
 export DAALA_ROOT=daala/
 
-echo Building...
-case $CODEC in
-  daala)
-    pushd $DAALA_ROOT
-    gcc -print-prog-name=cc1
-    gcc -print-search-dirs
-    ./autogen.sh; ./configure --enable-static --disable-shared --disable-player --disable-dump-images --enable-logging --enable-dump-recons $BUILD_OPTIONS; make -j4
-    popd
-    ;;
-  thor | thor-rt)
-    pushd $CODEC
-    make
-    popd
-    ;;
-  x264)
-    pushd x264/
-    ./configure $BUILD_OPTIONS
-    make
-    popd
-    ;;
-  x265)
-    pushd x265/build/linux
-    cmake -D ENABLE_SHARED=no $BUILD_OPTIONS ../../source/
-    make
-    popd
-    ;;
-  vp10 | vp10-rt)
-    pushd $CODEC
-    ./configure --enable-vp10 $BUILD_OPTIONS
-    make
-    popd
-    ;;
-  av1 | av1-rt)
-    pushd $CODEC
-    ./configure --enable-av1 --disable-unit-tests --disable-docs $BUILD_OPTIONS
-    make
-    popd
-    ;;
-  vp9)
-    pushd vp9
-    ./configure --enable-vp9 $BUILD_OPTIONS
-    make
-    popd
-    ;;
-esac
+./build_codec.sh
 
 if [ "$QUALITIES" ]; then
   QUALITY_OPTS="-qualities $QUALITIES"
