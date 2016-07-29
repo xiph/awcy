a74dccd0c20ff10bdc40ae1e691a86975ce52613
Guillaume Martres <smarter@ubuntu.com>
2016-07-29 01:41:18 -0400

    Use 13 aws machines by default

--- run_video_test.sh
+++ run_video_test.sh
@@ -13,8 +13,8 @@ if [ "$QUALITIES" ]; then
 fi
 
 cd rd_tool
-#DAALA_ROOT=../daala python3 -u rd_tool.py -machines 14 -awsgroup "AOM" -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" "$3"
-DAALA_ROOT=../daala python3 -u rd_tool.py -machineconf localhost.json -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" -bindir "../runs/$2/x86_64" "$3"
+DAALA_ROOT=../daala python3 -u rd_tool.py -machines 13 -awsgroup "AOM" -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" "$3"
+#DAALA_ROOT=../daala python3 -u rd_tool.py -machineconf localhost.json -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" -bindir "../runs/$2/x86_64" "$3"
 if [ "$AB_COMPARE" ]; then
-  DAALA_ROOT=../daala python3 -u rd_tool.py -codec $CODEC -awsgroup "AOM" -mode 'ab' -runid "$RUN_ID" "$3"
+  DAALA_ROOT=../daala python3 -u rd_tool.py -machines 13 -codec $CODEC -awsgroup "AOM" -mode 'ab' -runid "$RUN_ID" "$3"
 fi
