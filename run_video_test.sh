5c5d5541544e15737c0d6a9d314ee49eb7bc6f79
Thomas Daede <daede003@umn.edu>
2016-08-11 11:20:42 -0700

    Add runid to command line parameters to rd_tool.py.

--- run_video_test.sh
+++ run_video_test.sh
@@ -13,7 +13,7 @@ if [ "$QUALITIES" ]; then
 fi
 
 cd rd_tool
-DAALA_ROOT=../daala python3 -u rd_tool.py -machines "$NUM_MACHINES" -awsgroup "$SCALING_GROUP" -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" -bindir "../runs/$2/x86_64" "$3"
+DAALA_ROOT=../daala python3 -u rd_tool.py -machines "$NUM_MACHINES" -awsgroup "$SCALING_GROUP" -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" -runid "$RUN_ID" -bindir "../runs/$2/x86_64" "$3"
 #DAALA_ROOT=../daala python3 -u rd_tool.py -machineconf localhost.json -codec $CODEC $QUALITY_OPTS -prefix "../runs/$2/$3" -bindir "../runs/$2/x86_64" "$3"
 if [ "$AB_COMPARE" ]; then
   DAALA_ROOT=../daala python3 -u rd_tool.py -machines "$NUM_MACHINES" -codec $CODEC -awsgroup "$SCALING_GROUP" -mode 'ab' -runid "$RUN_ID" -bindir "../runs/$2/x86_64" "$3"
