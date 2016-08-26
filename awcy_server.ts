40a56b450adbc36c575506fd2803d04e56462b7e
Thomas Daede <daede003@umn.edu>
2016-08-25 17:26:29 -0700

    Run generate_list after build.

--- awcy_server.ts
+++ awcy_server.ts
@@ -140,6 +140,7 @@ function add_to_run_queue(job) {
     console.log(error);
     console.log(body);
   });
+  cp.exec('node generate_list.js');
 }
 
 express.static.mime.define({'text/plain': ['out']});
