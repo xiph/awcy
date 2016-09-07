156ab5dd457f23f0e2752c27bd09f89dcf3194c5
Thomas Daede <daede003@umn.edu>
2016-09-07 15:51:47 -0700

    Use analyzer master branch.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -4,7 +4,7 @@ set -e
 
 # git remote should be https://github.com/mbebenita/aomanalyzer.git
 git fetch analyzer
-git merge --no-commit -s recursive -X theirs analyzer/accounting-analyzer
+git merge --no-commit -s recursive -X theirs analyzer/master
 make distclean
 
 echo Building Analyzer
