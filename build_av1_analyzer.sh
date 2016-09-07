17dfb9dc495a6fc7da578ab8782123d57cef1376
Thomas Daede <daede003@umn.edu>
2016-09-07 14:48:49 -0700

    Always configure analyzer.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -8,17 +8,15 @@ git merge --no-commit -s recursive -X theirs analyzer/accounting-analyzer
 make distclean
 
 echo Building Analyzer
-if [ ! -d "asm" ]; then
-  echo Configuring Analyzer
-  mkdir asm
-  cd asm && emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting
-fi
 
-cd asm
+echo Configuring Analyzer
+mkdir -p asm
+pushd asm
+emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting
 emmake make
 cp examples/analyzer_decoder examples/analyzer_decoder.bc
 emcc -O3 examples/analyzer_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../ins/post.js" --memory-init-file 0
-cd ..
+popd
 mkdir -p ins/bin
 cp asm/examples/decoder.js ins/bin/decoder.js
 cp asm/examples/decoder.js.mem ins/bin/decoder.js.mem
