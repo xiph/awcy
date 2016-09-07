28a69840cafe924f13c28c7f524d19531fabd690
Thomas Daede <daede003@umn.edu>
2016-09-07 14:40:56 -0700

    Build analyzer without mem file.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -6,4 +6,19 @@ set -e
 git fetch analyzer
 git merge --no-commit -s recursive -X theirs analyzer/accounting-analyzer
 make distclean
-./build_analyzer.sh
+
+echo Building Analyzer
+if [ ! -d "asm" ]; then
+  echo Configuring Analyzer
+  mkdir asm
+  cd asm && emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting
+fi
+
+cd asm
+emmake make
+cp examples/analyzer_decoder examples/analyzer_decoder.bc
+emcc -O3 examples/analyzer_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../ins/post.js" --memory-init-file 0
+cd ..
+mkdir -p ins/bin
+cp asm/examples/decoder.js ins/bin/decoder.js
+cp asm/examples/decoder.js.mem ins/bin/decoder.js.mem
