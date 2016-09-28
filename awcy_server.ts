430eac7d612ab14278ad53f7d087a8857a4f504b
Thomas Daede <daede003@umn.edu>
2016-09-28 13:58:23 -0700

    Allow CORS on POST as well

--- awcy_server.ts
+++ awcy_server.ts
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 };
