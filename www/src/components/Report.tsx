d4dff01da2a4b14b0614d46f16674f7fd6f363de
Michael Bebenita <mbebenita@gmail.com>
2016-10-18 14:23:58 -0700

    Add Copy to Clipboard support.

--- www/src/components/Report.tsx
+++ www/src/components/Report.tsx
@@ -6,6 +6,7 @@ import { BDRateReport, Report, AppStore, Jobs, Job, JobStatus, loadXHR, ReportFi
 declare var require: any;
 
 let Select = require('react-select');
+let copyToClipboard = require('copy-to-clipboard');
 
 function formatNumber(n) {
   return n.toLocaleString(); // .replace(/\.00$/, '');
@@ -135,6 +136,67 @@ export class BDRateReportComponent extends React.Component<BDRateReportProps, {
     this.setState({reversed: !this.state.reversed} as any);
     this.loadReport({a: report.b, b: report.a});
   }
+  onCopyToClipboardClick() {
+    function padTable(rows: any [][]) {
+      let numCols = rows[0].length;
+      let maxColWidths = new Uint32Array(numCols);
+      for (let i = 0; i < numCols; i++) {
+        for (let j = 0; j < rows.length; j++) {
+          maxColWidths[i] = Math.max(maxColWidths[i], rows[j][i].length);
+        }
+      }
+      function padLeft(s, l, c) {
+        while (s.length < l)
+            s = c + s;
+        return s;
+      }
+      function padRight(s, l, c) {
+        while (s.length < l)
+            s = s + c;
+        return s;
+      }
+      for (let i = 0; i < numCols; i++) {
+        for (let j = 0; j < rows.length; j++) {
+          rows[j][i] = padLeft(rows[j][i], maxColWidths[i], ' ');
+        }
+      }
+    }
+
+    let a = this.props.a;
+    let b = this.props.b;
+    let report = this.state.report;
+    let summaryHeaders = ["PSNR", "PSNR Cb", "PSNR Cr", "PSNR HVS", "SSIM", "MS SSIM", "CIEDE 2000"];
+    let summaryRows = [summaryHeaders];
+    summaryRows.push(summaryHeaders.map(name => report.average[name].toFixed(2)));
+    padTable(summaryRows);
+
+    let text = report.a.id + " -> " + report.b.id + "\n\n";
+    text += summaryRows.map(row => row.join(" | ")).join("\n");
+
+    function toRow(video: string, data) {
+      return [video].concat(report.metricNames.map(name => data[name].toFixed(2)));
+    }
+    let rowHeaders = ["Video"].concat(report.metricNames);
+    let rows = [rowHeaders];
+    for (let video in report.metrics) {
+      rows.push(toRow(video, report.metrics[video]));
+    }
+    padTable(rows);
+    text += "\n\n";
+    text += rows.map(row => row.join(" | ")).join("\n");
+
+    // Markdown
+    text += "\n\nMarkdown Version\n\n";
+    summaryRows.splice(1, 0, summaryHeaders.map(() => "---"));
+    padTable(summaryRows);
+    text += summaryRows.map(row => `| ${row.join(" | ")} |`).join("\n");
+
+    text += "\n\n";
+    rows.splice(1, 0, rowHeaders.map(() => "---"));
+    padTable(rows);
+    text += rows.map(row => `| ${row.join(" | ")} |`).join("\n");
+    copyToClipboard(text);
+  }
   render() {
     // console.debug("Rendering BDRateReport");
     let a = this.props.a;
@@ -168,7 +230,8 @@ export class BDRateReportComponent extends React.Component<BDRateReportProps, {
     }
     return <Panel header={`BD Rate Report ${report.a.selectedName + " " + report.a.id} → ${report.b.selectedName + " " + report.b.id}`}>
       <div style={{ paddingBottom: 8, paddingTop: 4 }}>
-        <Button active={this.state.reversed} onClick={this.onReverseClick.bind(this)} >Reverse</Button>
+        <Button active={this.state.reversed} onClick={this.onReverseClick.bind(this)} >Reverse</Button>{' '}
+        <Button active={this.state.reversed} onClick={this.onCopyToClipboardClick.bind(this)} >Copy to Clipboard</Button>
       </div>
       <Table striped bordered condensed hover style={{width: "100%"}}>
         <thead>
