8e11f31151ac59e7da56d99c297445f941472c63
Thomas Daede <daede003@umn.edu>
2016-11-16 10:28:31 -0800

    Allow runs with colons in their name.

--- create_test_branch.sh
+++ create_test_branch.sh
@@ -8,11 +8,13 @@ TESTNAME="$2"
 
 CODEC="$3"
 
+BRANCH=t-$(echo "$TESTNAME" | sed "s/:/_/g")
+
 cd $CODEC
 git fetch --all
 git reset --hard
 git checkout $COMMIT
-git checkout -b t-$TESTNAME
+git checkout -b $BRANCH
 git clean -d -x -f
 
 cd ..
