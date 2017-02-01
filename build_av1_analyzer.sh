ca6d054517be363d9dfcda3dab4f7db34cdc1e82
Thomas Daede <daede003@umn.edu>
2017-01-31 16:32:36 -0800

    Revert "Update to new analyzer branch"
    This reverts commit 75d8bf59f15c0e80a22774994641cc0143535e91.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -4,7 +4,7 @@ set -e
 
 # git remote should be https://github.com/mbebenita/aomanalyzer.git
 git fetch analyzer
-git merge --no-commit -s recursive -X theirs analyzer/aom-analyzer
+git merge --no-commit -s recursive -X theirs analyzer/master
 make distclean
 
 echo Building Analyzer
@@ -12,9 +12,10 @@ echo Building Analyzer
 echo Configuring Analyzer
 mkdir -p asm
 pushd asm
-emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-analyzer $BUILD_OPTIONS
-emmake make -j2
-cp aomanalyzer aomanalyzer.bc
-emcc -O3 aomanalyzer.bc -o aomanalyzer.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../aomanalyzer-post.js" --memory-init-file 0
+emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting $BUILD_OPTIONS
+emmake make
+cp examples/analyzer_decoder examples/analyzer_decoder.bc
+emcc -O3 examples/analyzer_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../ins/post.js" --memory-init-file 0
 popd
-cp asm/aomanalyzer.js ./aomanalyzer.js
+mkdir -p ins/bin
+cp asm/examples/decoder.js ins/bin/decoder.js
