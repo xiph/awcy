b1da526494e421daf9914e0138a87a300c1ab721
Thomas Daede <daede003@umn.edu>
2017-02-01 17:01:47 -0800

    Build both old and new analyzer

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -9,7 +9,7 @@ make distclean
 
 echo Building Analyzer
 
-echo Configuring Analyzer
+echo Configuring Old ABI Analyzer
 mkdir -p asm
 pushd asm
 emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting $BUILD_OPTIONS
@@ -19,3 +19,18 @@ emcc -O3 examples/analyzer_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134
 popd
 mkdir -p ins/bin
 cp asm/examples/decoder.js ins/bin/decoder.js
+
+echo Configuring New ABI Analyzer
+
+make distclean
+git reset --hard
+git merge --no-commit -s recursive -X theirs analyzer/aom-analyzer
+
+mkdir -p asm
+pushd asm
+emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-analyzer $BUILD_OPTIONS
+emmake make -j2
+cp aomanalyzer aomanalyzer.bc
+emcc -O3 aomanalyzer.bc -o aomanalyzer.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../aomanalyzer-post.js" --memory-init-file 0
+popd
+cp asm/aomanalyzer.js ./aomanalyzer.js
