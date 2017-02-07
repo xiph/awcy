d7ece0a0cc860026806cba9fb25cf5fbde5fb5fb
Thomas Daede <daede003@umn.edu>
2017-02-07 15:11:52 -0800

    Remove old analyzer ABI generation

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -9,22 +9,6 @@ make distclean
 
 echo Building Analyzer
 
-echo Configuring Old ABI Analyzer
-mkdir -p asm
-pushd asm
-emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --disable-docs --disable-webm-io --extra-cflags="-D_POSIX_SOURCE" $BUILD_OPTIONS
-emmake make -j2
-cp examples/analyzer_decoder examples/analyzer_decoder.bc
-emcc -O3 examples/analyzer_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../ins/post.js" --memory-init-file 0
-popd
-mkdir -p ins/bin
-cp asm/examples/decoder.js ins/bin/decoder.js
-
-echo Configuring New ABI Analyzer
-
-git reset --hard
-git merge --no-commit -s recursive -X theirs analyzer/aom-analyzer
-rm -rf asm/
 mkdir -p asm
 pushd asm
 emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-analyzer --disable-docs --disable-webm-io --extra-cflags="-D_POSIX_SOURCE" $BUILD_OPTIONS
