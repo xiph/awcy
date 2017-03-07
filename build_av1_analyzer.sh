e70d7f695194e359b1ec827ac797f1a43c5dd23c
Thomas Daede <daede003@umn.edu>
2017-03-07 10:41:13 -0800

    Build new mainlined inspector.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-# git remote should be https://github.com/mbebenita/aomanalyzer.git
-git fetch analyzer
-git merge --no-commit -s recursive -X theirs analyzer/master
-make distclean || true
-
 echo Building Analyzer
 
 mkdir -p asm
 pushd asm
-emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-analyzer --disable-docs --disable-webm-io --extra-cflags="-D_POSIX_SOURCE" $BUILD_OPTIONS
+emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-inspection --disable-docs --disable-webm-io --extra-cflags="-D_POSIX_SOURCE" $BUILD_OPTIONS
 emmake make -j4
-cp aomanalyzer aomanalyzer.bc
-emcc -O3 aomanalyzer.bc -o aomanalyzer.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../aomanalyzer-post.js" --memory-init-file 0
+cp examples/inspect inspect.bc
+emcc -O3 inspect.bc -o inspect.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../tools/inspect-post.js" --memory-init-file 0
 popd
-cp asm/aomanalyzer.js ./aomanalyzer.js
+cp asm/inspect.js ./aomanalyzer.js
