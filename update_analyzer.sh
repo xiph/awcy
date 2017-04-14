78c7f03d4fcc85a78b93798c9c9ed0387f158054
Thomas Daede <daede003@umn.edu>
2017-04-14 14:44:31 -0700

    Disable win32 builds until I can do it without wine.

--- update_analyzer.sh
+++ update_analyzer.sh
@@ -5,8 +5,8 @@ npm run build-release
 echo Building downloadable analyzers
 npm run package-linux
 npm run package-darwin
-npm run package-win32
+#npm run package-win32
 cd release_builds
 tar -czf AOMAnalyzer-linux-x64.tar.gz AOMAnalyzer-linux-x64/
 tar -czf AOMAnalyzer-darwin-x64.tar.gz AOMAnalyzer-darwin-x64/
-zip -r AOMAnalyzer-win32-x64.zip AOMAnalyzer-win32-x64/
+#zip -r AOMAnalyzer-win32-x64.zip AOMAnalyzer-win32-x64/
