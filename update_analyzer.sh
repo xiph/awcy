eaa13a5bbfc78e48ba7bb5e3f52b5a96bc59a0db
Thomas Daede <daede003@umn.edu>
2017-04-14 14:12:21 -0700

    Add analyzer building.

--- update_analyzer.sh
+++ update_analyzer.sh
@@ -2,3 +2,11 @@
 cd ../aomanalyzer
 git pull
 npm run build-release
+echo Building downloadable analyzers
+npm run package-linux
+npm run package-darwin
+npm run package-win32
+cd release_builds
+tar -czf AOMAnalyzer-linux-x64.tar.gz AOMAnalyzer-linux-x64/
+tar -czf AOMAnalyzer-darwin-x64.tar.gz AOMAnalyzer-darwin-x64/
+zip -r AOMAnalyzer-win32-x64.zip AOMAnalyzer-win32-x64/
