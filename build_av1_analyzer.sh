23d3014de4a8782bed64c210b3597e6192e4388d
Thomas Daede <daede003@umn.edu>
2017-04-28 16:05:31 -0700

    Double decoder memory.

--- build_av1_analyzer.sh
+++ build_av1_analyzer.sh
@@ -11,6 +11,6 @@ pushd asm
 emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-inspection --disable-docs --disable-webm-io --extra-cflags="-D_POSIX_SOURCE" $BUILD_OPTIONS
 emmake make -j4
 cp examples/inspect inspect.bc
-emcc -O3 inspect.bc -o inspect.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../tools/inspect-post.js" --memory-init-file 0
+emcc -O3 inspect.bc -o inspect.js -s TOTAL_MEMORY=268435456 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../tools/inspect-post.js" --memory-init-file 0
 popd
 cp asm/inspect.js ./aomanalyzer.js
