b10281eafbec0ab26d2e9b0771fa24c4fd8044bf
Thomas Daede <daede003@umn.edu>
2017-05-02 11:36:57 -0700

    Skip fetch if commit is already available locally.

--- create_test_branch.sh
+++ create_test_branch.sh
@@ -11,9 +11,14 @@ CODEC="$3"
 BRANCH=t-$(echo "$TESTNAME" | sed "s/:/_/g")
 
 cd $CODEC
-git fetch --all
 git reset --hard
-git checkout $COMMIT
+if git checkout $COMMIT; then
+    echo "Commit found, skipping fetch."
+else
+    git fetch --all
+    git checkout $COMMIT
+fi
+
 git checkout -b $BRANCH
 git clean -d -x -f
 
