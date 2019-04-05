6b5e9c112c85679d92cac54f1d68500e5bebd0c4
Thomas Daede <daede003@umn.edu>
2019-04-05 15:09:04 -0700

    Update number of jobs to 4096

--- www/src/stores/Stores.ts
+++ www/src/stores/Stores.ts
@@ -903,7 +903,7 @@ export class AppStore {
           return (new Date(b.date) as any) - (new Date(a.date) as any);
         });
         let changed = false;
-        json = json.slice(0, 2048);
+        json = json.slice(0, 4096);
         json.forEach(o => {
           let job = this.findJob(o.run_id);
           if (job) {
