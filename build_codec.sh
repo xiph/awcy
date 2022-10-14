683754d965ca19d7c6e78e5cb51d698f4ff3c47c
Vibhoothi <mindfreeze@videolan.org>
2022-10-14 02:15:58 +0100

    build: Build SVT-AV1 as static binary

--- build_codec.sh
+++ build_codec.sh
@@ -125,7 +125,7 @@ case "${CODEC}" in
   svt-av1)
     cd ${CODECS_SRC_DIR}/svt-av1
     cd Build/linux
-    ./build.sh --cc=gcc --cxx=g++
+    ./build.sh --cc=gcc --cxx=g++ --release --static
     ;;
 
   vvc-vtm)
